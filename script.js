1dc986ee6f6595700c4e82d72efbbd7735ac0ff5
Laykajenta <marianne.odegaard@outlook.com>
2025-07-12 22:38:24 +0200

    lagt til function for å endre bilde

---
+++ script.js
@@ -0,0 +1,24 @@
+// navigation
+/* const nav = document.querySelector('.nav');
+const menuIcon = document.querySelector('.menu-icon');
+
+function toggleMenu() {
+    nav.classList.toggle('active');
+    menuIcon.classList.toggle('active');
+}
+
+function hideMenu() {
+    nav.classList.remove('active');
+    menuIcon.classList.remove('active');
+} */
+
+// slideshow
+let currentImageIndex = 0;
+const images = document.querySelectorAll('.slide');
+
+function switchImage() {
+    images[currentImageIndex].classList.remove('active');
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    images[currentImageIndex].classList.add('active');
+}
+setInterval(switchImage, 5000);
